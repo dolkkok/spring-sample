ee6d362fcdb2de11ba14c1dd25034716e96f7428
jhkwon78 <jhkwon78@tmon.co.kr>
2017-03-31 16:31:42 +0900

    .

--- template/app/src/main/java/com/tmon/App.java
+++ template/app/src/main/java/com/tmon/App.java
@@ -1,22 +1,21 @@
 package com.tmon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+import java.util.Properties;
+
 public class App {
-    public static void main(String[] args) {
-        for (String s : args) {
-            System.out.println(s);
-        }
-        
-        System.out.println( "Hello World!" );
-       
+
+    public static void main(String[] args) throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
+
+        Properties prop = new Properties();
+        prop.load(HelloWorld.class.getClassLoader().getResourceAsStream("data.properties"));
+        System.out.println("Mode : " + prop.getProperty("mode"));
     }
 }
