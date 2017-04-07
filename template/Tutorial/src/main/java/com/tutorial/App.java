dc9da9f8da9d29bcd4f9fffc62ee5ee2a8df17ce
JinHoKwon <aswip78@gmail.com>
2017-04-07 23:01:04 +0900

    .

--- template/Tutorial/src/main/java/com/tutorial/App.java
+++ template/Tutorial/src/main/java/com/tutorial/App.java
@@ -10,6 +10,7 @@ import org.elasticsearch.action.exists.ExistsRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
@@ -17,9 +18,12 @@ import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHitField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.context.annotation.*;
 import org.springframework.expression.spel.ast.Indexer;
 import org.springframework.stereotype.Component;
@@ -27,6 +31,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+
+/// https://www.elastic.co/guide/en/elasticsearch/client/java-api/1.3/query-dsl-filters-caching.html
 
 @Configuration
 @ComponentScan({"com.tmon"})
@@ -138,12 +147,14 @@ public class App {
     /// 데이터 조회
     public void searchAllDocument(Client client) {
 
-        SearchResponse r = client.prepareSearch()
+        SearchRequestBuilder builder = client.prepareSearch()
                 .setIndices(getIndexName())
                 .setTypes(getTypeName())
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.matchAllQuery())
-                .execute().actionGet();
+                .setQuery(QueryBuilders.matchAllQuery());
+
+        System.out.println(builder.internalBuilder());
+        SearchResponse r = builder.execute().actionGet();
 
         String name, age, memo;
 
@@ -167,18 +178,18 @@ public class App {
 
     public void searchAllDocumentWithField(Client client) {
 
-        SearchResponse r = client.prepareSearch()
+        SearchRequestBuilder builder = client.prepareSearch()
                 .setIndices(getIndexName())
                 .setTypes(getTypeName())
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
                 .addFields("name", "age", "memo")
-                .setQuery(QueryBuilders.matchAllQuery())
-                .execute().actionGet();
+                .setQuery(QueryBuilders.matchAllQuery());
 
+        System.out.println(builder.internalBuilder());
+        SearchResponse r = builder.execute().actionGet();
         String name, age, memo;
 
         for (SearchHit hit : r.getHits()) {
-
             hit.fields().forEach((key, val) -> {
                 System.out.print(key + " : " + val.<String>getValue() + ", ");
             });
@@ -195,6 +206,237 @@ public class App {
         }
     }
 
+    public void searchTermQuery(Client client) {
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(termQuery("name", "tomas"))
+                .setFrom(1)
+                .setSize(2);
+
+        doQuery(builder);
+    }
+
+
+    public void searchBoolQuery(Client client) {
+        /// 반드시 포함 : name = tomas, age = 10
+        /// 반드시 불포함 : age < 30 and age > 40
+        /// OR 조건 : memo = 'hello' or memo = 'am'
+        QueryBuilder qb = QueryBuilders
+                .boolQuery()
+                .must(termQuery("name", "tomas"))
+                .must(termQuery("age", "10"))
+                .mustNot(rangeQuery("age").from(30).to(40))
+                .should(termQuery("memo", "hello"))
+                .should(termQuery("memo", "am"));
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public  void matchQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.matchQuery("name", "tomas");
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void multiMatchQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.multiMatchQuery(
+                "hello tomas",     // Text you are looking for
+                "name", "memo"           // Fields you query on
+        );
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void idsQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.idsQuery().ids("1", "2", "4");
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb)
+                .addSort("age", SortOrder.ASC);
+             // .addSort(SortBuilders.fieldSort("name").order(SortOrder.ASC));
+
+        doQuery(builder);
+    }
+
+    public void constantScoreQuery(Client client) {
+        /// With Queries
+        QueryBuilder qb = QueryBuilders.constantScoreQuery(QueryBuilders.termQuery("name","james"))
+                .boost(2.0f);
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void constantScoreQueryFilter(Client client) {
+        /// Using with Filters
+        QueryBuilder qb = QueryBuilders.constantScoreQuery(FilterBuilders.termFilter("name", "tomas"))
+                .boost(2.0f);
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void prefixQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.prefixQuery("name", "tom");
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void searchRangeQuery(Client client) {
+        QueryBuilder qb = QueryBuilders
+                        .rangeQuery("age")
+                        .from(10)
+                        .to(30)
+                        .includeLower(true)
+                        .includeUpper(false);
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void searchTermsQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.termsQuery("memo",    // field
+                                "hello", "am")               // values
+                                .minimumMatch(1);              // How many terms must match
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void searchWildCardQuery(Client client) {
+        QueryBuilder qb = QueryBuilders.wildcardQuery("name", "t?ma*");
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(qb);
+
+        doQuery(builder);
+    }
+
+    public void searchAndFilter(Client client) {
+        FilterBuilder fb = FilterBuilders.andFilter(
+                FilterBuilders.rangeFilter("age").from("10").to("30"),
+                FilterBuilders.prefixFilter("name", "tom")
+        );
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setPostFilter(fb);
+
+        doQuery(builder);
+    }
+
+    public void searchBoolFilter(Client client) {
+        FilterBuilder fb = FilterBuilders.boolFilter()
+                .must(FilterBuilders.termFilter("name", "tomas"))
+                .mustNot(FilterBuilders.rangeFilter("age").from("40").to("100"))
+                .should(FilterBuilders.termFilter("memo", "am"))
+                .should(FilterBuilders.termFilter("memo", "2017"));
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setPostFilter(fb);
+
+        doQuery(builder);
+    }
+
+    public void searchOrFilter(Client client) {
+        FilterBuilder fb = FilterBuilders.orFilter(
+                FilterBuilders.termFilter("name", "tomas"),
+                FilterBuilders.termFilter("name", "james")
+        );
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setPostFilter(fb);
+
+        doQuery(builder);
+    }
+
+    public void searchQueryFilterCache(Client client) {
+        FilterBuilder fb = FilterBuilders.andFilter(
+                    FilterBuilders.rangeFilter("age").from("10").to("20"),
+                    FilterBuilders.prefixFilter("name", "tom")
+                ).cache(true);
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setPostFilter(fb);
+
+        doQuery(builder);
+    }
+
+    /*
+    public void searchFilteredQuery(Client client) {
+        FilteredQueryBuilder filteredQueryBuilder =
+                QueryBuilders.filteredQuery(QueryBuilders.termQuery("name", "tomas"),
+                                            FilterBuilders.termFilter("name","tomas"));
+
+        SearchRequestBuilder builder = client.prepareSearch(getIndexName())
+                .setTypes(getTypeName())
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(filteredQueryBuilder);
+
+        doQuery(builder);
+    }*/
+
+    public void doQuery(SearchRequestBuilder builder) {
+        System.out.println(builder.internalBuilder());
+        SearchResponse r = builder.get();
+
+        for (SearchHit hit : r.getHits()) {
+            hit.getSource().forEach((key, val) -> {
+                System.out.print(key + " : " + val + ", ");
+            });
+            System.out.println();
+        }
+    }
+
+
+
     public static void main( String[] args ) {
 
         AnnotationConfigApplicationContext context = null;
@@ -208,15 +450,33 @@ public class App {
             //app.existIndex(client);
             //app.deleteIndex(client);
 
-            app.insertDocument(client, "1", "Tomas", "10", "Hello Tomas 2017");
-            app.insertDocument(client, "2", "James", "20", "Hello James 2018");
-            app.insertDocument(client, "3", "Lukas", "30", "Hello Lukas 2019");
-
-            app.deleteDocument(client, "3");
-            app.updateDocument(client, "2", "James Dean", "20", "Hello James 2018");
+//            app.insertDocument(client, "1", "Tomas", "10", "Hello Tomas 2017");
+//            app.insertDocument(client, "2", "James", "20", "Hello James 2018");
+//            app.insertDocument(client, "3", "Lukas", "30", "Hello Lukas 2019");
+//            app.insertDocument(client, "4", "Tomas", "40", "Hello Tomas 2020");
+//            app.insertDocument(client, "5", "Tomas", "10", "I Am Tomas 2020");
+//
+//            app.deleteDocument(client, "3");
+//            app.updateDocument(client, "2", "James Dean", "20", "Hello James 2018");
 
             //app.searchAllDocument(client);
-            app.searchAllDocumentWithField(client);
+            //app.searchAllDocumentWithField(client);
+            //app.searchTermQuery(client);
+            //app.searchFilteredQuery(client);
+            //app.searchBoolQuery(client);
+            //app.matchQuery(client);
+            //app.multiMatchQuery(client);
+            //app.idsQuery(client);
+            //app.constantScoreQuery(client);
+            //app.constantScoreQueryFilter(client);
+            //app.prefixQuery(client);
+            //app.searchRangeQuery(client);
+            //app.searchTermsQuery(client);
+            //app.searchWildCardQuery(client);
+            //app.searchAndFilter(client);
+            //app.searchBoolFilter(client);
+            //app.searchOrFilter(client);
+            app.searchQueryFilterCache(client);
             client.close();
         } catch (final Exception e) {
             e.printStackTrace();
